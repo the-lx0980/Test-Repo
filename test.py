7acb874b7d3fa37c8afd996cc5559609035bf8b0
Protullen <138602811+protullen@users.noreply.github.com>
2024-05-29 12:10:08 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,20 @@
 from pyrogram import Client, filters
 from os import environ
 
-bot = Clinet(
-    api_id = int(environ.get('ID'))
-    api_hash = environ.get('HASH')
-    bot_token = environ.get('TOKEN')
+app_id = int(environ.get('ID'))
+api_hash = environ.get('HASH')
+bot_token = environ.get('TOKEN')
+
+bot = Client(    
+    name='test-bot',
+    api_id=app_id,
+    api_hash=api_hash,
+    bot_token=bot_token
 )
 
+@bot.on_message(filters.command('start'))
+async def start(bot, message):
+    await message.reply('Alive! ------->  Zinda hai!!!!')
 
-
+print('Bot Started!')
+bot.run()
